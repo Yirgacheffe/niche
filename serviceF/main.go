1b1d1fb4c6d2693f45c90dcb2f7d912df03b7ae1
Aaron Yu <aaron.yuu@outlook.com>
2019-11-05 11:03:52 +0800

    Add service f init functions.

--- serviceF/main.go
+++ serviceF/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"os"
+
 	runtime "github.com/banzaicloud/logrus-runtime-formatter"
 	log "github.com/sirupsen/logrus"
 )
 
+func getEnv(key, fallback string) string {
+
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+
+}
+
 func init() {
+
+	formatter := runtime.Formatter{ChildFormatter: &log.JSONFormatter{}}
+	formatter.Line = true
+
+	log.SetFormatter(&formatter)
+	log.SetOutput(os.Stdout)
+
+	level, err := log.ParseLevel(getEnv("LOG_LEVEL", "info"))
+	if err != nil {
+		log.Error(err)
+	}
+
+	log.SetLevel(level)
 
 }
 

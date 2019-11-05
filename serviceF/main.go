d5a3ad275c9eb6f84e02b741aa6fd34bd0db5ec0
Aaron Yu <aaron.yuu@outlook.com>
2019-11-05 23:55:03 +0800

    Coding complete for service f.

--- serviceF/main.go
+++ serviceF/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/gorilla/mux"
+
+	"github.com/google/uuid"
+
 	runtime "github.com/banzaicloud/logrus-runtime-formatter"
 	log "github.com/sirupsen/logrus"
+	"github.com/streadway/amqp"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Greeting message transparent between service in json format
@@ -21,10 +29,31 @@ type Greeting struct {
 
 var greetings []Greeting
 
-func PingHandler() {
+// PingHandler ...
+func PingHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	greetings = nil
+
+	tmpGreeting := Greeting{
+		ID:          uuid.New().String(),
+		ServiceName: "Service-F",
+		Message:     "Hola, from Service-F!",
+		CreatedAt:   time.Now().Local(),
+	}
+
+	greetings = append(greetings, tmpGreeting)
+	CallMongoDB(tmpGreeting)
+
+	err := json.NewEncoder(w).Encode(greetings)
+	if err != nil {
+		log.Error(err)
+	}
 
 }
 
+// HealthCheckHandler ...
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-type", "application/json; charset=utf-8")
@@ -33,6 +62,82 @@ func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Error(err)
 	}
+
+}
+
+func CallMongoDB(greeting Greeting) {
+
+	log.Info(greeting)
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_CONN")))
+	if err != nil {
+		log.Error(err)
+	}
+
+	defer client.Disconnect(nil)
+
+	collection := client.Database("service-f").Collection("messages")
+	ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+
+	_, err = collection.InsertOne(ctx, greeting)
+	if err != nil {
+		log.Error(err)
+	}
+
+}
+
+func GetMessages() {
+
+	conn, err := amqp.Dial(os.Getenv("RABBITMQ_CONN"))
+	if err != nil {
+		log.Error(err)
+	}
+
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Error(err)
+	}
+
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"service-d",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Error(err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"service-f",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Error(err)
+	}
+
+	forever := make(chan bool)
+
+	go func() {
+		for delivery := range msgs {
+			log.Debug(delivery)
+			CallMongoDB(deserialization(delivery.Body))
+		}
+	}()
+
+	<-forever
 
 }
 
@@ -82,9 +187,13 @@ func init() {
 
 func main() {
 
-	formatter := runtime.Formatter{ChildFormatter: &log.JSONFormatter{}}
-	formatter.Line = true
+	go GetMessages()
 
-	log.SetFormatter(&formatter)
+	router := mux.NewRouter()
+	api := router.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/ping", PingHandler).Methods("GET")
+	api.HandleFunc("/health", HealthCheckHandler).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 }

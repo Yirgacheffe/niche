d14d4e53dd9e9675bafd07f2ec96c0af39292813
Aaron Yu <aaron.yuu@outlook.com>
2019-11-05 11:35:07 +0800

    Add deserilization codes.

--- serviceF/main.go
+++ serviceF/main.go
@@ -1,11 +1,57 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
 	"os"
+	"time"
 
 	runtime "github.com/banzaicloud/logrus-runtime-formatter"
 	log "github.com/sirupsen/logrus"
 )
+
+// Greeting message transparent between service in json format
+type Greeting struct {
+	ID          string    `json:"id,omitempty"`
+	ServiceName string    `json:"service,omitempty"`
+	Message     string    `json:"message,omitempty"`
+	CreatedAt   time.Time `json:"created,omitempty"`
+}
+
+var greetings []Greeting
+
+func PingHandler() {
+
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-type", "application/json; charset=utf-8")
+
+	_, err := w.Write([]byte("{\"alive\": true}"))
+	if err != nil {
+		log.Error(err)
+	}
+
+}
+
+func deserialization(b []byte) Greeting {
+
+	log.Debug(b)
+
+	buf := bytes.NewBuffer(b)
+	decoder := json.NewDecoder(buf)
+
+	var tmpGreeting Greeting
+	err := decoder.Decode(&tmpGreeting)
+	if err != nil {
+		log.Error(err)
+	}
+
+	return tmpGreeting
+
+}
 
 func getEnv(key, fallback string) string {
 

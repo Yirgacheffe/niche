1323f4b78ebdabf1cc652f8aa9d51b4bd71c6c54
Aaron Yu <aaron.yuu@outlook.com>
2019-11-20 15:32:39 +0800

    Move template file out of this projects.

--- deploy/install/mongodb.md
+++
@@ -1,7 +0,0 @@
-# REF: https://github.com/helm/charts/tree/master/stable/mongodb
-
-# MongoDB
-helm install --name codefly-mongodb stable/mongodb
-
-# Get Password
-kubectl get secret --namespace default ${secret_name} -o jsonpath="{.data.mongodb-root-password}" | base64 --decode
--- deploy/port_forwards.sh
+++
@@ -1,9 +0,0 @@
-# RabbitMQ
-kubectl port-forward -n default svc/codefly-rabbitmq 15672:15672
-
-# Greetings Service D
-kubectl port-forward -n niche-dev svc/niche-greetings-d 8090:8080
-
-# MongoDB
-kubectl port-forward --namespace default svc/codefly-mongodb 27017:27017 &
-mongo --host 127.0.0.1 --authenticationDatabase admin -p $MONGODB_ROOT_PASSWORD

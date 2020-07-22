5b5084c1c35a0dafa93ad7df3221acf39d98d958
Aaron Yu <aaron.yuu@outlook.com>
2020-07-22 14:44:59 +0800

    Add endpoint test, not succeed yet.

---
+++ todox/endpoints_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetNotes(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/api/notes", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ListNoteHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned a wrong status code: got %v expected %v",
+			status,
+			http.StatusOK)
+	}
+
+	expected := "{}"
+	got := rr.Body.String()
+
+	if got != expected {
+		t.Errorf("Handler returned a unexpected result: got %v expected %v",
+			got,
+			expected)
+	}
+
+}

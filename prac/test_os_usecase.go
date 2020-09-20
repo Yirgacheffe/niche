c75c097e3b8790a5dea788e28ad000b3c650f7fe
Aaron Yu <aaron.yuu@outlook.com>
2020-09-20 13:51:19 +0800

    Add os expand function demo.

--- prac/test_os_usecase.go
+++ prac/test_os_usecase.go
@@ -18,6 +18,7 @@ func main() {
 
 	fmt.Printf("os.DevNull => %v\n", os.DevNull)
 
+	// os env
 	envs := os.Environ()
 	fmt.Printf("os.Environ => %v\n", envs)
 
@@ -26,5 +27,41 @@ func main() {
 
 	orange, oExists := os.LookupEnv("ORANGE")
 	fmt.Printf("os.LookupEnv(\"ORANGE\") => %v (exists: %v)\n", orange, oExists)
+
+	err := os.Setenv("FRUIT", "apple")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	apple := os.Getenv("FRUIT")
+	fmt.Printf("os.Getenv(\"FRUIT\") => %s\n", apple)
+
+	err = os.Unsetenv("FRUIT")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	apple = os.Getenv("FRUIT")
+	fmt.Printf("os.Getenv(\"FRUIT\") => %s\n", apple)
+
+	// os expand, expand env
+	// Replace the place holder
+	expdMapper := func(ph string) string {
+		switch ph {
+		case "FRUIT":
+			return "mongo"
+		case "CAR":
+			return "VW"
+		default:
+			return "<empty>"
+		}
+	}
+
+	raw := "I am eating $FRUIT and driving ${CAR}"
+	formatted := os.Expand(raw, expdMapper)
+
+	fmt.Println(formatted)
+
+	//
 
 }

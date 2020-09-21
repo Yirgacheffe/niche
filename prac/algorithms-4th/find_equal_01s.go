030553f9486c8096d67545604fb3590228f8e294
Aaron Yu <aaron.yuu@outlook.com>
2020-09-21 16:09:11 +0800

    Update hashmap func for equal 0,1.

--- prac/algorithms-4th/find_equal_01s.go
+++ prac/algorithms-4th/find_equal_01s.go
@@ -2,15 +2,8 @@ package main
 
 import "fmt"
 
-type PairIdx struct {
-	start int
-	end   int
-}
-
 func replaceZero(a []int) []int {
-
 	tmp := []int{}
-
 	// Initial the temp array
 	for _, v := range a {
 		if v == 0 {
@@ -19,16 +12,14 @@ func replaceZero(a []int) []int {
 			tmp = append(tmp, v)
 		}
 	}
-
 	return tmp
-
 }
 
 func bruteForce(a []int) {
 
-	var p PairIdx
-	n := len(a)
+	startIdx := 0
 
+	n := len(a)
 	maxlen := 0
 
 	for i := 0; i < n; i++ {
@@ -40,22 +31,66 @@ func bruteForce(a []int) {
 			if sum == 0 {
 				tmpLen := j - i + 1
 				if maxlen <= tmpLen {
+					startIdx = i
 					maxlen = tmpLen
-					p = PairIdx{start: i, end: j}
 				}
 			}
 		}
-
 	}
 
 	fmt.Printf("Max lenth is: %d\n", maxlen)
-	fmt.Printf("Index is: %d -> %d\n", p.start, p.end)
+	fmt.Printf("Index is: %d -> %d\n", startIdx, maxlen)
+
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+// // Using map store the sum - index pair
+func betterForce(a []int) {
+
+	hmap := make(map[int]int)
+	sum := 0
+	maxlen := 0
+	endIdx := 0
+
+	for i, v := range a {
+		sum += v
+
+		// Handle sum == 0 at last index
+		if sum == 0 {
+			maxlen = i + 1
+			endIdx = i
+		}
+
+		if _, ok := hmap[sum]; ok {
+
+			if i-hmap[sum] > maxlen {
+				maxlen = i - hmap[sum]
+				endIdx = i
+			}
+
+		} else {
+			hmap[sum] = i
+		}
+	}
+
+	startIdx := endIdx - maxlen + 1
+
+	fmt.Println(startIdx)
+	fmt.Println(endIdx)
 
 }
 
 func main() {
 	a := []int{1, 0, 1, 1, 1, 0, 0}
-
 	tmp := replaceZero(a)
+
 	bruteForce(tmp)
+	betterForce(tmp)
 }

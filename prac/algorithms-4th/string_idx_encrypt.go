38531e9944662127b5f45ae5c0fc9539eaffd0b4
Aaron Yu <aaron.yuu@outlook.com>
2020-09-22 11:06:28 +0800

    Add index encrypt demo case.

---
+++ prac/algorithms-4th/string_idx_encrypt.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func encryptWithIdx(s string) string {
+
+	n := len(s)
+	if n > 128 {
+		fmt.Errorf("Max size is 128, return back.")
+		return s
+	}
+
+	chars := []byte(s)
+	j := 0
+
+	for i := (n - 1); i >= 0; i-- {
+		chars[j] = (s[i] + byte(i)) % 128
+		j++
+	}
+
+	return string(chars)
+
+}
+
+func main() {
+	s0 := "geeks"
+	s1 := "java"
+
+	fmt.Println(encryptWithIdx(s0))
+	fmt.Println(encryptWithIdx(s1))
+}

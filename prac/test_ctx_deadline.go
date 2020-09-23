534ee1793c431b5eb358709a42e6067862b4c19f
Aaron Yu <aaron.yuu@outlook.com>
2020-09-23 15:04:57 +0800

    add context with deadline case.

---
+++ prac/test_ctx_deadline.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"time"
+)
+
+var startTime = time.Now()
+
+func worker(ctx context.Context, seconds int) {
+
+	select {
+	case <-ctx.Done():
+		fmt.Printf("%0.2fs - worker(%d) killed !!!\n", time.Since(startTime).Seconds(), seconds)
+		return
+	case <-time.After(time.Duration(seconds) * time.Second):
+		// This case is weird, default case to run ???
+		fmt.Printf("%0.2fs - worker(%d) completed.\n", time.Since(startTime).Seconds(), seconds)
+	}
+
+}
+
+func main() {
+
+	deadline := time.Now().Add(3 * time.Second)
+	ctx, cancel := context.WithDeadline(context.Background(), deadline)
+
+	// mannually cancel if main returned before deadline
+	// let's say if return in 1 second
+	defer cancel()
+
+	go worker(ctx, 2)
+
+	// These 2 will be killed as deadline is 3 seconds
+	go worker(ctx, 6)
+	go worker(ctx, 8)
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("Number of goroutine", runtime.NumGoroutine())
+
+}

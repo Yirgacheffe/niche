aa9254da856ebf6f44d8e7bf266d7674c1d18d18
Aaron Yu <aaron.yuu@outlook.com>
2020-10-10 10:07:59 +0800

    Finish the method add and get.

--- prac/net/rpc/models.go
+++ prac/net/rpc/models.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Student reprent student ...
 type Student struct {
 	ID        int
@@ -17,13 +21,31 @@ type College struct {
 }
 
 // Add method add student into the college
-func (c *College) Add() error {
-	return nil
+func (c *College) Add(payload Student, reply *Student) error {
+
+	id := payload.ID
+	if _, ok := c.database[id]; ok {
+		return fmt.Errorf("Student with id %d already exists", id)
+	}
+
+	c.database[id] = payload
+	reply = &payload
+
+	return nil // Return nil without error
+
 }
 
 // Get method return student with that id
-func (c *College) Get(id int) error {
-	return nil
+func (c *College) Get(payload int, reply *Student) error {
+
+	s, ok := c.database[payload]
+	if !ok {
+		return fmt.Errorf("No student found with id %d", payload)
+	}
+
+	*reply = s
+	return nil // Return nil without error
+
 }
 
 // NewCollege function return a college contains the students

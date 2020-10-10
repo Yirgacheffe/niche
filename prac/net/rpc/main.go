607de7bc1720bb10c480236b00c94b47e2153ca0
Aaron Yu <aaron.yuu@outlook.com>
2020-10-10 10:51:49 +0800

    Add rpc server main.

--- prac/net/rpc/main.go
+++ prac/net/rpc/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/rpc"
+)
 
 func main() {
+
 	fmt.Println("RPC Server started...")
+
+	// In general ways, should be 'package.NewCollege()'
+	mit := NewCollege()
+
+	rpc.Register(mit)
+	rpc.HandleHTTP()
+	// rpc.HandleHTTP(rpcPath, debugPath string)
+
+	http.HandleFunc(
+		"/",
+		func(w http.ResponseWriter, r *http.Request) {
+			io.WriteString(w, "RPC Server Live!")
+		},
+	)
+
+	http.ListenAndServe(":9093", nil)
+
 }

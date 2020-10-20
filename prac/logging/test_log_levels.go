5a22b827567f0dbe7a1db4d3bf7586ea9856469c
Aaron Yu <aaron.yuu@outlook.com>
2020-10-20 14:38:00 +0800

    Complete the log levels cases.

--- prac/logging/test_log_levels.go
+++ prac/logging/test_log_levels.go
@@ -15,22 +15,41 @@ const (
 	FATAL
 )
 
-func init() {
-	// Initialize the loggers
-	// InfoLogger
-	// WarningLogger
-	// ErrorLogger
-	// FatalLogger
+// Infologger, WarningLogger, ErrorLogger, FatalLogger definition
+// Init them following ...
+var (
+	InfoLogger    *log.Logger
+	WarningLogger *log.Logger
+	ErrorLogger   *log.Logger
+	FatalLogger   *log.Logger
+)
 
-	// Lutc | Lmicroseconds | Llongfile
+func init() {
+
+	file, err := os.OpenFile("levels.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal("levels.log not exist!")
+	}
+
+	// The `file` is a `writer` actually
+	// Show different log options
+	InfoLogger = log.New(file, "INFO: ", log.LUTC|log.Lmicroseconds|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	FatalLogger = log.New(file, "FATAL: ", log.LUTC|log.Lmicroseconds|log.Llongfile)
+
 }
 
 func main() {
-	writeLog(INFO, "this is information message!")
-	writeLog(WARNING, "this is warning message!")
+	InfoLogger.Println("This is information message!")
 
+	ErrorLogger.Println("This is Error message!")
+	FatalLogger.Println("This is Fatal message!")
+
+	WarningLogger.Println("Will this line show in log file?")
 }
 
+/*
 func writeLog(msgtype messageType, msg string) {
 
 	file, err := os.OpenFile("levels.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
@@ -56,3 +75,4 @@ func writeLog(msgtype messageType, msg string) {
 	}
 
 }
+*/

0af05ffd4ff20dee63d09e447f9440f39859ad0c
Aaron Yu <aaron.yuu@outlook.com>
2020-10-28 10:54:07 +0800

    Add copy dir demo.

--- prac/cli/envconfig/main.go
+++ prac/cli/envconfig/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 func main() {
 	fmt.Println("WIP......")
 
+	b, _ := ioutil.ReadAll(strings.NewReader("dkfjsk"))
+
+	fmt.Println(b)
 }

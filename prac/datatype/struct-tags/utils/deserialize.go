1e89344d6de3fba14b5482c475bd209de40cef01
Aaron Yu <aaron.yuu@outlook.com>
2020-11-16 15:26:23 +0800

    do deserialize code demo.

--- prac/datatype/struct-tags/utils/deserialize.go
+++ prac/datatype/struct-tags/utils/deserialize.go
@@ -1,8 +1,23 @@
 package utils
 
+import (
+	"errors"
+	"reflect"
+)
+
 // DeserializeStructStrings converts a serialized string using our custom
 // serialization format to a struct
 func DeserializeStructStrings(s string, res interface{}) error {
+
+	r := reflect.TypeOf(res)
+
+	// using a pointer, check if it is correct
+	if r.Kind() != reflect.Ptr {
+		return errors.New("argument res must be a pointer")
+	}
+
+	r = r.Elem()
+	v := reflect.ValueOf(res).Elem()
 
 	return nil
 }

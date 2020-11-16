354ecd3c08da23a92ed24c8118946ed8c62fe746
Aaron Yu <aaron.yuu@outlook.com>
2020-11-16 17:08:53 +0800

    done with deserialize.go file.

--- prac/datatype/struct-tags/utils/deserialize.go
+++ prac/datatype/struct-tags/utils/deserialize.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"errors"
 	"reflect"
+	"strings"
 )
 
 // DeserializeStructStrings converts a serialized string using our custom
@@ -16,8 +17,40 @@ func DeserializeStructStrings(s string, res interface{}) error {
 		return errors.New("argument res must be a pointer")
 	}
 
+	// deref the pointer
 	r = r.Elem()
-	v := reflect.ValueOf(res).Elem()
+	value := reflect.ValueOf(res).Elem()
+
+	// split our serialization string into a map
+	vals := strings.Split(s, ";")
+	valMap := make(map[string]string)
+
+	for _, v := range vals {
+		keyv := strings.Split(v, ":")
+		if len(keyv) != 2 {
+			continue
+		}
+		valMap[keyv[0]] = keyv[1]
+	}
+
+	// loop over the fields
+	for i := 0; i < r.NumField(); i++ {
+		field := r.Field(i)
+
+		if serialize, ok := field.Tag.Lookup("serialize"); ok {
+			// "-" will be ignored
+			if serialize == "-" {
+				continue
+			}
+
+			if val, ok := valMap[serialize]; ok {
+				value.Field(i).SetString(val)
+			}
+		} else if val, ok := valMap[field.Name]; ok {
+			value.Field(i).SetString(val)
+		}
+	}
 
 	return nil
+
 }

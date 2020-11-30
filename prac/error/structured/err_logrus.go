fe30898fed1616b71fb4a3f88671540dc9a7ba37
Aaron Yu <aaron.yuu@outlook.com>
2020-11-30 15:57:55 +0800

    add logrus and apex demos.

---
+++ prac/error/structured/err_logrus.go
@@ -0,0 +1,38 @@
+package main
+
+import "github.com/sirupsen/logrus"
+
+type Hook struct {
+	id string
+}
+
+// Fire will trigger whenever you log
+func (h *Hook) Fire(entry *logrus.Entry) error {
+	entry.Data["id"] = hook.id
+	return nil
+}
+
+// Levels is what level the hook will fire on
+func (h *Hook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+// Logrus demonstrates some basic logrus functionality
+func Logrus() {
+
+	logrus.SetFormatter(&logrus.TextFormatter{})
+	logrus.SetLevel(logrus.InfoLevel)
+	logrus.AddHook(&Hook{"123"})
+
+	fields := logrus.Fields{}
+	fields["success"] = true
+	fields["complex_struct"] = struct {
+		Event string
+		When  string
+	}{"Something happened", "Just now"}
+
+	x := logrus.WithFields(fields)
+	x.Warn("warning!")
+	x.Error("error!")
+
+}

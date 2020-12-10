56e3e1d185be9c7ea7a6221937f2096b0553fb69
Aaron Yu <aaron.yuu@outlook.com>
2020-12-10 11:07:11 +0800

    simple demo for grpc.

--- prac/grpc/chatdemo/server.go
+++ prac/grpc/chatdemo/server.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 
+	"./chat"
 	"google.golang.org/grpc"
 )
 
 func main() {
+
+	fmt.Println("Go gRPC beginner turorial!")
 
 	listener, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	s := chat.Server{}
 	server := grpc.NewServer()
-	if err := grpcServer.Serve(listener); err != nil {
+
+	chat.RegisterChatServiceServer(server, &s)
+
+	if err := server.Serve(listener); err != nil {
 		log.Fatalf("failed to server: %s", err)
 	}
 

c82b45628e92b2c3e1c8e6e22a07b550b03c2511
Aaron Yu <aaron.yuu@outlook.com>
2020-12-14 23:01:46 +0800

    add test required version case.

--- prac/os/test_required_ver.go
+++ prac/os/test_required_ver.go
@@ -3,12 +3,25 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
 	curVersion := runtime.Version()
 
-	fmt.Println(curVersion)
+	major := strings.Split(curVersion, ".")[0][2]
+	minor := strings.Split(curVersion, ".")[1]
+
+	m1, _ := strconv.Atoi(string(major))
+	m2, _ := strconv.Atoi(minor)
+
+	if m1 == 1 && m2 < 8 {
+		fmt.Println("Need Go version 1.8 or higher!")
+		return
+	}
+
+	fmt.Println("You are using Go version 1.8 or higher!")
 
 }

7180c8d8131d957fa8c2e31890fdab49e9654181
Aaron Yu <aaron.yuu@outlook.com>
2021-01-20 15:59:56 +0800

    add impl of grpc service.

---
+++ prac/grpc/bitbucket-repos/internal/grpc/impl/repository-service.go
@@ -0,0 +1,10 @@
+package impl
+
+//RepositoryServiceGrpcImpl is a implementation of RepositoryService Grpc Service.
+type RepositoryServiceGrpcImpl struct {
+}
+
+//NewRepositoryServiceGrpcImpl returns the pointer to the implementation.
+func NewRepositoryServiceGrpcImpl() *RepositoryServiceGrpcImpl {
+	return &RepositoryServiceGrpcImpl{}
+}

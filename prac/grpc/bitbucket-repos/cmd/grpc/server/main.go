4f9723544a12eef4b4422c51f156c63a6582032b
Aaron Yu <aaron.yuu@outlook.com>
2021-01-20 16:40:16 +0800

    add grpc server.

--- prac/grpc/bitbucket-repos/cmd/grpc/server/main.go
+++ prac/grpc/bitbucket-repos/cmd/grpc/server/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bitbucket-repos/internal/grpc/impl"
+	"bitbucket-repos/internal/grpc/service"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("gRPC in progress......")
+
+	netListener := getNetListener(7000)
+	grpcServ := grpc.NewServer()
+
+	repoServiceImpl := impl.NewRepositoryServiceGrpcImpl()
+	service.RegisterRepositoryServiceServer(grpcServ, repoServiceImpl)
+
+	if err := grpcServ.Serve(netListener); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+
+}
+
+func getNetListener(port uint) net.Listener {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+		panic(fmt.Sprintf("failed to listen: %v", err))
+	}
+
+	return listener
 }
--- prac/grpc/bitbucket-repos/internal/grpc/impl/repository-service.go
+++ prac/grpc/bitbucket-repos/internal/grpc/impl/repository-service.go
@@ -1,5 +1,15 @@
 package impl
 
+import (
+	"bitbucket-repos/internal/grpc/domain"
+	"bitbucket-repos/internal/grpc/service"
+	"context"
+	"log"
+	"strconv"
+	// "../domain"
+	// "../service"
+)
+
 //RepositoryServiceGrpcImpl is a implementation of RepositoryService Grpc Service.
 type RepositoryServiceGrpcImpl struct {
 }
@@ -7,4 +17,17 @@ type RepositoryServiceGrpcImpl struct {
 //NewRepositoryServiceGrpcImpl returns the pointer to the implementation.
 func NewRepositoryServiceGrpcImpl() *RepositoryServiceGrpcImpl {
 	return &RepositoryServiceGrpcImpl{}
+}
+
+// Add function implementation of gRPC service.
+func (serviceImpl *RepositoryServiceGrpcImpl) Add(ctx context.Context, in *domain.Repository) (*service.AddRepositoryRresponse, error) {
+
+	log.Println("Received request for adding repository with id " + strconv.FormatInt(in.Id, 10))
+	log.Println("Repository persisted to the storage")
+
+	return &service.AddRepositoryResponse{
+		AddedRepository: in,
+		Error:           nil,
+	}, nil
+
 }

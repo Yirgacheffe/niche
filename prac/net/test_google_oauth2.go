95591c811a39ec442870c665dcb9e1b8f5abfa41
Aaron Yu <aaron.yuu@outlook.com>
2021-01-28 23:00:08 +0800

    add google oauth2 client demo.

--- prac/net/test_google_oauth2.go
+++ prac/net/test_google_oauth2.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"google.golang.org/api/people/v1"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -54,9 +55,51 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 }
 
 func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf()
+	fmt.Printf("Saving credential file to: %s\n", path)
+
+	f, err := os.Openfile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
 
 func main() {
-	fmt.Println("WIP .......")
+	
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	config, err := google.ConfigFromJSON(b, people.ContactsReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	client := getClient(config)
+
+	srv, err := people.New(client)
+	if err != nil {
+		log.Fatalf("Unable to create people Client %v", err)
+	}
+
+	r, err := srv.People.Connections.List("people/me").PageSize(10).PersonFields("names,emailAddress").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve people. %v", err)
+	}
+	
+	if len(r.Connections) > 0 {
+		fmt.Print("List 10 connection name:\n")
+		for _, c := range r.Connections {
+			names := c.Names
+			if len(names) > 0 {
+				name := names[0].DisplayName
+				fmt.Printf("%s\n", name)
+			}
+		}
+	} else {
+		fmt.Println("No connections found.")
+	}
+
 }

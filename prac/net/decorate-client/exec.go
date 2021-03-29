6f8971a633fea233026f08dcd927d767131440d1
Aaron Yu <aaron.yuu@outlook.com>
2021-03-29 18:17:10 +0800

    add http client decorator

---
+++ prac/net/decorate-client/exec.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func Exec() error {
+	c := Setup()
+
+	resp, err := c.Get("https://www.google.com")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Response code:", resp.StatusCode)
+	return nil
+
+}
---
+++ prac/net/decorate-client/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	if err := Exec(); err != nil {
+		panic(err)
+	}
+}

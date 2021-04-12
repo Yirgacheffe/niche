bff8de6a266901c5c5ee551ef2f2849464f8a4b4
Aaron Yu <aaron.yuu@outlook.com>
2021-04-12 15:36:05 +0800

    add bi-linkedlist add node function.

--- prac/data-structure/test_doubly_linkedlist.go
+++ prac/data-structure/test_doubly_linkedlist.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+type Node struct {
+	Previous *Node
+	Value    int
+	Next     *Node
+}
+
+var root = new(Node)
+
+func addNode(t *Node, v int) int {
+
+	if t == nil {
+		t = &Node{nil, v, nil}
+		root = t
+		return 0
+	}
+
+	if v == t.Value {
+		fmt.Println("Node already exists:", v)
+		return -1
+	}
+
+	if t.Next == nil {
+		temp := t
+		t.Next = &Node{temp, v, nil}
+		return -2
+	}
+
+	return addNode(t.Next, v)
+
+}
+
 func main() {
 	fmt.Println("Hello world!")
 }

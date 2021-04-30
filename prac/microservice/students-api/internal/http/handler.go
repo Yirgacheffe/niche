2b3753e554832e243fbc6ba72418a9900fb2d63c
Aaron Yu <aaron.yuu@outlook.com>
2021-04-30 19:18:23 +0800

    add api handler for tag and due date.

--- prac/microservice/students-api/internal/http/handler.go
+++ prac/microservice/students-api/internal/http/handler.go
@@ -26,10 +26,15 @@ func (h *Handler) InitRoutes() {
 
 	h.Router.HandleFunc("/api/students", h.GetAllStudents).Methods("GET")
 	h.Router.HandleFunc("/api/students", h.PostStudent).Methods("POST")
+	h.Router.HandleFunc("/api/students/{id:[0-9]+}", h.GetStudentByID).Methods("GET")
+	h.Router.HandleFunc("/api/students/{id:[0-9]+}", h.DeleteStudent).Methods("DELETE")
+	h.Router.HandleFunc("/api/tag/{tag}/", h.GetStudentsByTag).Methods("GET")
+	h.Router.HandleFunc("/api/due/{year:[0-9]+}/{month:[0-9]+}/{day:[0-9]+}", h.GetStudentsByDate).Methods("GET")
 
 	h.Router.HandleFunc("/api/status", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Status Up!")
 	})
+
 }
 
 func (h *Handler) GetAllStudents(w http.ResponseWriter, r *http.Request)     {}
@@ -38,6 +43,8 @@ func (h *Handler) GetStudentBySchool(w http.ResponseWriter, r *http.Request) {}
 func (h *Handler) GetStudentByID(w http.ResponseWriter, r *http.Request)     {}
 func (h *Handler) UpdateStudent(w http.ResponseWriter, r *http.Request)      {}
 func (h *Handler) DeleteStudent(w http.ResponseWriter, r *http.Request)      {}
+func (h *Handler) GetStudentsByTag(w http.ResponseWriter, r *http.Request)   {}
+func (h *Handler) GetStudentsByDate(w http.ResponseWriter, r *http.Request)  {}
 
 func renderJSON(w http.ResponseWriter, v interface{}) {
 

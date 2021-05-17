bcc2a472dee290b38debabe7243416f6df734d45
Aaron Yu <aaron.yuu@outlook.com>
2021-05-17 13:54:56 +0800

    add middleware function.

--- microservice/middleware/handler.go
+++ microservice/middleware/handler.go
@@ -1,8 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"log"
+	"net/http"
+	"runtime/debug"
+	"time"
+)
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Success!!"))
+}
+
+func Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf("%s %s %s", r.Method, r.RequestURI, time.Since(start))
+	})
+}
+
+func PanicRecovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				log.Println(string(debug.Stack()))
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+const UserContextKey = "user"
+
+func BasicAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+
+		if ok && authdb.VerifyUserPass(user, pass) {
+			newCtx := context.WithValue(r.Context(), UserContextKey, user)
+			next.ServeHTTP(w, r.WithContext(newCtx))
+		} else {
+			w.Header().Set(
+				"WWW-Authenticate", `Basic realm="api"`,
+			)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		}
+	})
 }

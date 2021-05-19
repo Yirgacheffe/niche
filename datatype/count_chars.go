94ca354e21759a1e54790fd98ec9b4f8568dad60
Aaron Yu <aaron.yuu@outlook.com>
2021-05-19 22:56:41 +0800

    add map and slice demo.

---
+++ datatype/count_chars.go
@@ -0,0 +1,58 @@
+// Example from book 'The go programming language', count chars...
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+
+	var utflen [utf8.UTFMax + 1]int
+	invalid := 0
+	counts := make(map[rune]int)
+
+	in := bufio.NewReader(os.Stdin)
+
+	for {
+		r, n, err := in.ReadRune()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "count_chars: %v\n", err)
+			os.Exit(1)
+		}
+
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+
+		counts[r]++
+		utflen[n]++
+	}
+
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+
+}

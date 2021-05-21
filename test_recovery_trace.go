debf2cab2be074b4050278ec6041fd3ae9cac892
Aaron Yu <aaron.yuu@outlook.com>
2021-05-22 01:14:01 +0800

    refactor the structure.

--- test_recovery_trace.go
+++ test_recovery_trace.go
@@ -1,32 +1,64 @@
 package main
 
-import (  
-    "fmt"
-    "runtime/debug"
+import (
+	"fmt"
+	"runtime/debug"
+
+	"golang.org/x/net/html"
 )
 
-func recoverFullName() {  
-    if r := recover(); r != nil {
-        fmt.Println("recovered from ", r)
-        debug.PrintStack()
-    }
+func soleTitle(doc *html.Node) (title string, err error) {
+	type bailout struct{}
+
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+			// no panic found
+		case bailout{}:
+			err = fmt.Errorf("multiple title elements")
+		default:
+			panic(p) // unexpected panic
+		}
+	}()
+
+	forEachNode(doc, func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" && n.FirstChild != nil {
+			if title != "" {
+				panic(bailout{}) // multiple title
+			}
+			title = n.FirstChild.Data
+		}
+	}, nil)
+
+	if title == "" {
+		return "", fmt.Errorf("no title element")
+	}
+
+	return title, nil
 }
 
-func fullName(firstName *string, lastName *string) {  
-    defer recoverFullName()
-    if firstName == nil {
-        panic("runtime error: first name cannot be nil")
-    }
-    if lastName == nil {
-        panic("runtime error: last name cannot be nil")
-    }
-    fmt.Printf("%s %s\n", *firstName, *lastName)
-    fmt.Println("returned normally from fullName")
+func recoverFullName() {
+	if r := recover(); r != nil {
+		fmt.Println("recovered from ", r)
+		debug.PrintStack()
+	}
 }
 
-func main() {  
-    defer fmt.Println("deferred call in main")
-    firstName := "Elon"
-    fullName(&firstName, nil)
-    fmt.Println("returned normally from main")
+func fullName(firstName *string, lastName *string) {
+	defer recoverFullName()
+	if firstName == nil {
+		panic("runtime error: first name cannot be nil")
+	}
+	if lastName == nil {
+		panic("runtime error: last name cannot be nil")
+	}
+	fmt.Printf("%s %s\n", *firstName, *lastName)
+	fmt.Println("returned normally from fullName")
+}
+
+func main() {
+	defer fmt.Println("deferred call in main")
+	firstName := "Elon"
+	fullName(&firstName, nil)
+	fmt.Println("returned normally from main")
 }

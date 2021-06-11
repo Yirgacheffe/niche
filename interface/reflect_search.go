31e0b898ce5e37579a519b4437644f12a44b9348
Aaron Yu <aaron.yuu@outlook.com>
2021-06-12 00:55:47 +0800

    test movie struct reflection test.

--- interface/reflect_search.go
+++ interface/reflect_search.go
@@ -167,3 +167,37 @@ func formatAtom(v reflect.Value) string {
 		return v.Type().String() + " value"
 	}
 }
+
+type Movie struct {
+	Title, Subtitle string
+	Year            int
+	Color           bool
+	Actor           map[string]string
+	Oscars          []string
+	Sequel          *string
+}
+
+var strangelove = Movie{
+	Title:    "Dr. Strangelove",
+	Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+	Year:     1964,
+	Color:    false,
+	Actor: map[string]string{
+		"Dr. Strangelove":            "Peter Sellers",
+		"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+		"Pres. Merkin Muffley":       "Peter Sellers",
+		"Gen. Buck Turgidson":        "George C. Scott",
+		"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+		`Maj. T.J. "king" Kong`:      "Slim Pickens",
+	},
+	Oscars: []string{
+		"Best Actor (Nomin.)",
+		"Best Adapted Screenplay (Nomin.)",
+		"Best Director (Nomin.)",
+		"Best Picture (Nomin.)",
+	},
+}
+
+func main() {
+	Display("strangelove", strangelove)
+}

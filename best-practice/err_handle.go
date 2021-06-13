ca2d31d1f8ffad9717d16dce1caea5d1712464bf
Aaron Yu <aaron.yuu@outlook.com>
2021-06-14 01:55:07 +0800

    add write response err handing.

--- best-practice/err_handle.go
+++ best-practice/err_handle.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 )
 
@@ -35,3 +36,67 @@ func CountLinesBetter(r io.Reader) (int, error) {
 	}
 	return lines, sc.Err()
 }
+
+// ----------------# ------------------------ #----
+type header struct {
+	key, value string
+}
+
+type status struct {
+	code   int
+	reason string
+}
+
+// not good enough error handling
+func writeResponse(w io.Writer, st status, headers []header, body io.Reader) error {
+	_, err := fmt.Fprintf(w, "HTTP/1.1 %d %s\n", st.code, st.reason)
+	if err != nil {
+		return err
+	}
+
+	for _, h := range headers {
+		_, err := fmt.Fprintf(w, "%s: %s\n", h.key, h.value)
+		if err != nil {
+			return err
+		}
+	}
+
+	if _, err := fmt.Fprintf(w, "\n"); err != nil {
+		return err
+	}
+
+	_, err = io.Copy(w, body)
+	return err
+}
+
+// better way for error handling
+type errWriter struct {
+	io.Writer
+	err error
+}
+
+func (e *errWriter) Write(buf []byte) (int, error) {
+	if e.err != nil {
+		return 0, e.err
+	}
+
+	var n int
+	n, e.err = e.Writer.Write(buf)
+	return n, nil
+}
+
+func anotherResponse(w io.Writer, st status, headers []header, body io.Reader) error {
+	ew := &errWriter{Writer: w}
+	fmt.Fprintf(ew, "HTTP/1.1 %d %s\n", st.code, st.reason)
+
+	for _, h := range headers {
+		fmt.Fprintf(ew, "%s: %s\n", h.key, h.value)
+	}
+
+	fmt.Fprintf(ew, "\n")
+
+	io.Copy(ew, body)
+	return ew.err
+}
+
+// ----------------# ------------------------ #----

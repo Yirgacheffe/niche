c3ba0736467edb9727d9da40f776db9809515414
Aaron Yu <aaron.yuu@outlook.com>
2021-06-18 00:18:11 +0800

    refactor codes.

--- algorithms-4th/quiz_solving/swap_ab_without_t.go
+++ algorithms-4th/quiz_solving/swap_ab_without_t.go
@@ -5,12 +5,18 @@ import "fmt"
 func main() {
 
 	a, b := 1, 3
-	fmt.Println(a, b)
+	fmt.Println("0: ", a, b)
 
 	b = a + b
 	a = b - a
 	b = b - a
 
-	fmt.Println(a, b)
+	fmt.Println("1: ", a, b)
+
+	a = a ^ b
+	b = a ^ b
+	a = a ^ b
+
+	fmt.Println("2: ", a, b)
 
 }
--- db/pq-prods/good_way_validate.go
+++ db/pq-prods/good_way_validate.go
@@ -1,50 +1,51 @@
 package user
 
 import (
-    "encoding/json"
-    
-    "gopkg.in/go-playground/validator.v9"
+	"encoding/json"
+	"net/http"
+
+	"gopkg.in/go-playground/validator.v9"
 )
 
 type createUserRequest struct {
-    Email string `json:"email" validate:"required,email"`
-    Name string `json:"name" validate:"required,min=2,max=50"`
-    Password string `json:"name" validate:"required,min=8"`
+	Email    string `json:"email" validate:"required,email"`
+	Name     string `json:"name" validate:"required,min=2,max=50"`
+	Password string `json:"name" validate:"required,min=8"`
 }
 
 type UserHandler struct {
-    validate *validator.Validate
-    userService Service
-    // all other dependencies
+	validate    *validator.Validate
+	userService Service
+	// all other dependencies
 }
 
-func NewUserHandler(s Service, v *validator.Validate, /* all other dependencies */) UserHandler {
-    return UserHandler{userService: s, validate: v}
+func NewUserHandler(s Service, v *validator.Validate /* all other dependencies */) UserHandler {
+	return UserHandler{userService: s, validate: v}
 }
 
 // Different frameworks do this differently. For simplicity let's assume
 // we have built-in http server
 func (h UserHandler) Create(req *http.Request) error {
-    input := createUserRequest{}
-    
-    // unmarshal incoming input into pre-defined structure
-    defer req.Body.Close()
-    if err := json.NewEncoder(req.Body).Encode(&input) {
-        return err
-    }
+	input := createUserRequest{}
 
-    // Just terminate the request if the input is not valid
-    if err := h.validate.Struct(input); err != nil {
-        return err
-    }
-    
-    // Map to domain entity
-    user := User {
-        Email: input.Email,
-        Name: input.Name,
-        Password: input.Password,
-    }
-    
-    // Finally pass the entity into service to get the job done!
-    return h.userService.Create(user)
+	// unmarshal incoming input into pre-defined structure
+	defer req.Body.Close()
+	if err := json.NewEncoder(req.Body).Encode(&input); err != nil {
+		return err
+	}
+
+	// Just terminate the request if the input is not valid
+	if err := h.validate.Struct(input); err != nil {
+		return err
+	}
+
+	// Map to domain entity
+	user := User{
+		Email:    input.Email,
+		Name:     input.Name,
+		Password: input.Password,
+	}
+
+	// Finally pass the entity into service to get the job done!
+	return h.userService.Create(user)
 }

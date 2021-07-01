b25333a088c55c105d74ef670e7f9d214a70c98b
Aaron Yu <aaron.yuu@outlook.com>
2021-07-01 15:54:50 +0800

    reorg file and directory.

--- cli/inner_struct_flags.go
+++ cli/inner_struct_flags.go
@@ -3,7 +3,42 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
+	"strings"
 )
+
+// CountTheWays is a custom type that will be read a flag
+type CountTheWays []int
+
+func (c *CountTheWays) String() string {
+	result := ""
+
+	for _, v := range *c {
+		if len(result) > 0 {
+			result += " ... "
+		}
+		result += fmt.Sprint(v)
+	}
+
+	return result
+}
+
+// Set will be used by the flag package
+func (c *CountTheWays) Set(value string) error {
+
+	values := strings.Split(value, ",")
+
+	for _, v := range values {
+		i, err := strconv.Atoi(v)
+		if err != nil {
+			return err
+		}
+		*c = append(*c, i)
+	}
+
+	return nil // Re, return nil as no error occured
+
+}
 
 // Config is the holder for our flags
 type Config struct {
@@ -44,4 +79,13 @@ func (c *Config) GetMessage() string {
 
 	msg = fmt.Sprintf("%s with a certainty of %d out of 10. Let me count the ways %s", msg, c.howAwesome, c.countTheWays.String())
 	return msg
+}
+
+func main() {
+	c := Config{}
+	c.Setup()
+
+	// generally call this from main
+	flag.Parse()
+	fmt.Println(c.GetMessage())
 }

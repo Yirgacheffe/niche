3b68bfb8510599c5a4a9ece718d524c2434f18ec
Aaron Yu <aaron.yuu@outlook.com>
2021-08-29 01:05:27 +0800

    add graceful shutdown func.

--- students-api/cmd/server/main.go
+++ students-api/cmd/server/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"flag"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"students-api/internal/db"
+	"time"
 
 	internalHttp "students-api/internal/http"
 	"students-api/internal/service/student"
 )
 
-func Run() error {
-	fmt.Println("Running App")
+func Run(wait time.Duration) error {
+	log.Println("running app")
 
 	conn, err := db.InitDatabase()
 	if err != nil {
@@ -26,17 +31,45 @@ func Run() error {
 	handler := internalHttp.NewHandler(service)
 	handler.InitRoutes()
 
-	if err := http.ListenAndServe(":9010", handler.Router); err != nil {
-		return err
+	srv := &http.Server{
+		Addr:         ":9010",
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      handler.Router,
 	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Well, block untile receive the signal
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+
+	srv.Shutdown(ctx)
+	log.Println("shutting down...")
+	os.Exit(0)
 
 	return nil
 }
 
 func main() {
-	err := Run()
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
+	err := Run(wait)
 	if err != nil {
-		fmt.Println("Error running app")
-		fmt.Println(err)
+		log.Fatal("error while start app", err)
 	}
 }

4587adbb023678b01e56aa5941a4281086b83beb
Aaron Yu <aaron.yuu@outlook.com>
2021-08-28 21:00:36 +0800

    add common method.

--- students-api/cmd/server/main_test.go
+++ students-api/cmd/server/main_test.go
@@ -51,6 +51,19 @@ func setUp() {
 	rr = httptest.NewRecorder()
 }
 
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
 func TestMain(m *testing.M) {
 	setUpTableEnv()
 	setUp()

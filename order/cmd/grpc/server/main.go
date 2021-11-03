830d4ca1cee889e33a5978d74f497e1998465f23
Aaron Yu <aaron.yuu@outlook.com>
2021-11-03 19:09:49 +0800

    demo of grpc interceptor.

--- order/cmd/grpc/server/main.go
+++ order/cmd/grpc/server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 
@@ -14,6 +16,48 @@ import (
 )
 
 var orderMap = make(map[string]ec.Order)
+
+// demo of:: Unary Interceptor
+func orderUnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+
+	log.Println("[Server Interceptor]", info.FullMethod)
+	log.Printf("Pre  Proc Message : %s", req)
+
+	m, err := handler(ctx, req)
+
+	log.Printf("Post Proc Message : %s", m)
+	return m, err
+}
+
+// demo of:: Server Stream interceptor
+type wrappedStream struct {
+	grpc.ServerStream
+}
+
+func newWrappedStream(s grpc.ServerStream) grpc.ServerStream {
+	return &wrappedStream{s}
+}
+
+func (w *wrappedStream) RecvMsg(m interface{}) error {
+	log.Printf("[Server Stream Interceptor Wrapper]"+"Receive a message (Type: %T) at %s", m, time.Now().Format(time.RFC3339))
+	return w.ServerStream.RecvMsg(m)
+}
+
+func (w *wrappedStream) SendMsg(m interface{}) error {
+	log.Printf("[Server Stream Interceptor Wrapper]"+"Sending a message (Type: %T) at %v", m, time.Now().Format(time.RFC3339))
+	return w.ServerStream.SendMsg(m)
+}
+
+func orderServerStreamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	log.Println("[Server Stream Interceptor] ", info.FullMethod)
+
+	err := handler(srv, newWrappedStream(ss))
+	if err != nil {
+		log.Printf("RPC failed with error %v", err)
+	}
+
+	return err
+}
 
 func main() {
 
@@ -29,7 +73,10 @@ func main() {
 
 	initSampleData()
 
-	srv := grpc.NewServer()
+	// srv := grpc.NewServer()
+	srv := grpc.NewServer(
+		grpc.UnaryInterceptor(orderUnaryServerInterceptor), grpc.StreamInterceptor(orderServerStreamInterceptor))
+
 	sv.RegisterProductServiceServer(srv, pb.NewProductServer())
 	ec.RegisterOrderManagementServer(srv, pb.NewOrderManagementServer(orderMap))
 

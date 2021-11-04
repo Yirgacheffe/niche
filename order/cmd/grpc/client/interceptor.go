e0acca28899d9b0b4916679523f769acb666b07c
Aaron Yu <aaron.yuu@outlook.com>
2021-11-04 17:38:58 +0800

    mv client interceptor to sep folder.

--- order/cmd/grpc/client/interceptor.go
+++ order/cmd/grpc/client/interceptor.go
@@ -8,8 +8,45 @@ import (
 	"google.golang.org/grpc"
 )
 
-func orderClientUnaryInterceptor()  {}
-func orderClientStreamInterceptor() {}
+func orderClientUnaryInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn,
+	invoker grpc.UnaryInvoker,
+	opts ...grpc.CallOption) error {
+
+	log.Println("Method: " + method)
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	log.Println(reply)
+	return err
+}
+
+func orderClientStreamInterceptor(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string,
+	streamer grpc.Streamer,
+	opts ...grpc.CallOption) (grpc.ClientStream, error) {
+
+	log.Println("[Client Interceptor] ", method)
+	s, err := streamer(ctx, desc, cc, method, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return newWrappedStream(s), nil
+}
+
+type wrappedStream struct {
+	grpc.ClientStream
+}
+
+func newWrappedStream(s grpc.ClientStream) grpc.ClientStream {
+	return &wrappedStream{s}
+}
+
+func (w *wrappedStream) SendMsg(m interface{}) error {
+	log.Println("[Client Stream Interceptor]"+"Send a message (Type: %T) at %v", m, time.Now().Format(time.RFC3339))
+	return w.ClientStream.SendMsg(m)
+}
+func (w *wrappedStream) RecvMsg(m interface{}) error {
+	log.Println("[Client Stream Interceptor]"+"Receive a message (Type: %T) at %v", m, time.Now().Format(time.RFC3339))
+	return w.ClientStream.RecvMsg(m)
+}
 
 //
 func WithClientInterceptor() grpc.DialOption {
